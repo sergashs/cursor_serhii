c9a6edc0b11b4510e127af8c6e66ce54faef86a8
sergashs <sergas123456@gmail.com>
2020-02-02 17:59:48 +0200

    fix

--- guardian.cursor.hw1/js/index.js
+++ guardian.cursor.hw1/js/index.js
@@ -7,25 +7,17 @@ let minPrice = Math.min(milkPrice, winePrice, showergelPrice); // мінімал
 const totalPrice = (milkPrice + winePrice + showergelPrice); // загальна ціна
 let integersTotalPrice = Math.floor(milkPrice + winePrice + showergelPrice) // загальна ціна цілим числом
 const ceilprice = (Math.ceil((totalPrice)*100)/100);
-// bulevoe - 
+// bulevoe -
 const dealing = (money - totalPrice); // здача
-
-
 //------advanced part-------------------------------------
+
 let message = `
   <h1>Advanced part</h1>
   <p>Max number - <strong>${maxPrice}</strong></p>
-  <p>Min number - <strong>${minValue}</strong></p>
+  <p>Min number - <strong>${minPrice}</strong></p>
   <p>Total - <strong>${totalPrice}</strong></p>
-  <p>Integer part of product price - <strong>${integerTotalPrice}</strong></p>
-  <p>Price sume ceiled to hundreds - <strong>${ceiledToHundred}</strong></p>
-  <p>${evenOddPrice} % 2 = ${evenOdd} is <strong>odd</strong></p>
-  <p>Rest from 500uah is - <strong>${rest}uah</strong></p>
-  <p>Everage number ${totalPrice}/3 = <strong>${everageValue}</strong></p>
-  <p>The sale is -  <strong>${salePercent}%</strong></p>
-  <p>Price with sale -  <strong>${priceWithSale}uah</strong></p>
-  <p>The lost profit price is: ${everageCheckPrice} - ${salePrice} = <strong>${lostProfit}</strong></p>
+  <p>Integer part of product price - <strong>${integersTotalPrice}</strong></p>
+  <p>Price sume ceiled to hundreds - <strong>${ceilprice}</strong></p>
 `;
 document.writeln(message);
 console.log(message);
-
