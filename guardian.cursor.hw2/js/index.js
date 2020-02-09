e42aa5e2290fcd070fe8eeab2013a8f2bc311d4e
sergashs <sergas123456@gmail.com>
2020-02-09 11:59:10 +0200

    updt

--- guardian.cursor.hw2/js/index.js
+++ guardian.cursor.hw2/js/index.js
@@ -1,25 +1,34 @@
-let numericStart = +(prompt('Число з якого починаємо рахувати ', 1));
-let numericFinish = +(prompt('Число на якому закінчуємо рахувати ', 100));
-let skip = confirm("Пропускати парні числа?");
+let numericStart = Number(prompt('Число з якого починаємо рахувати ', 1));
 
-
-if (numericStart && numericFinish) {
-    let result1 = 0;
-    let sum = 0;
-    
-    for (let i = numericStart; i<=numericFinish; i++) {
-        if ( skip && !( i % 2 == 0 )  ) {
-            result1 +=i;
-            i++;
-        } else {
-            sum +=i;
-        }
-    }
-
-    if(result1){
-      document.writeln(`Сума не парних чисел: <b>${result1}</b>`)
-    }else{
-      document.writeln(`Сумма всіх чисел: <b>${sum}</b>`)
-    }
-
+while(isNaN(numericStart)) {
+        alert('Ви ввели не число');
+        numericStart = Number(prompt('Число з якого починаємо рахувати ', 1));
 }
+
+let numericFinish = Number(prompt('Число на якому закінчуємо рахувати ', 100));
+
+while(isNaN(numericFinish)) {
+    alert('Ви ввели не число');
+    numericFinish = Number(prompt('Число з якого починаємо рахувати ', 1));
+}
+
+let skip = confirm("Пропускати парні числа?")
+
+let sum=0;
+
+for(let i = numericStart; i <= numericFinish;i++) { //5+6+7+8+9+10+11+12+13+14+15
+    if(skip===true) {
+        if(i%2!==0) {
+            sum=sum+i;
+        }
+    } else {
+        sum=sum+i;
+    }
+}
+
+document.write(`
+<p>Початок циклу <b>${numericStart}</b></p>
+<p>Кінець циклу <b>${numericFinish}</b></p>
+<p>Парні числа ${skip===true ? '<b>пропущені</b>': '<b>не пропущені</b>'}</p>
+<p>Сума циклу <b>${sum}</b></p>
+`);
