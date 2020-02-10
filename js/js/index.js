83931ac43960f0ea0e52d332967acf8f7291cbf9
sergashs <sergas123456@gmail.com>
2020-02-10 22:19:44 +0200

    s

---
+++ js/js/index.js
@@ -0,0 +1,15 @@
+//2 = pow function
+
+function two() {
+  let namber = document.getElementById("fun-2").value;
+  let prcnt = document.getElementById("fun-22").value;
+
+  {
+    namber = parseInt(namber);
+    pow = parseInt(prcnt);
+    let temp = (namber / 100) * prcnt;
+    document.getElementById("fun-2-label").innerHTML = temp;
+    document.getElementById("fun-2-label").innerHTML = temp;
+    two();
+  }
+}
