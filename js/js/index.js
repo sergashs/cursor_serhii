ecd4e5593788ea3ade923c69e90aaa773c116989
sergashs <sergas123456@gmail.com>
2020-02-10 22:36:03 +0200

    d

--- js/js/index.js
+++ js/js/index.js
@@ -4,12 +4,10 @@ function two() {
   let namber = document.getElementById("fun-2").value;
   let prcnt = document.getElementById("fun-22").value;
 
-  {
-    namber = parseInt(namber);
-    pow = parseInt(prcnt);
-    let temp = (namber / 100) * prcnt;
-    document.getElementById("fun-2-label").innerHTML = temp;
-    document.getElementById("fun-2-label").innerHTML = temp;
-    two();
-  }
+  namber = parseInt(namber);
+  pow = parseInt(prcnt);
+  let temp = (namber / 100) * prcnt;
+  document.getElementById("fun-2-label").innerHTML = temp;
+  document.getElementById("fun-2-label").innerHTML = temp;
+  two();
 }
