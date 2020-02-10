ae8787143e7cca1c7c90a90a614215d9ab56f2c0
sergashs <sergas123456@gmail.com>
2020-02-10 21:47:47 +0200

    s

--- guardian.cursor.hw1/js/index.js
+++ guardian.cursor.hw1/js/index.js
@@ -1,24 +1,26 @@
-const milkPrice = 15.678; 
-const winePrice = 123.965; 
-const showergelPrice = 90.2345; 
+const milkPrice = 15.678;
+const winePrice = 123.965;
+const showergelPrice = 90.2345;
 const money = 500;
 const maxPrice = Math.max(milkPrice, winePrice, showergelPrice); // максимальне число
 const minPrice = Math.min(milkPrice, winePrice, showergelPrice); // мінімальне число
-const totalPrice = (milkPrice + winePrice + showergelPrice); // загальна ціна
-const integersTotalPrice = Math.floor(milkPrice + winePrice + showergelPrice) // загальна ціна цілим числом
-const ceilprice = (Math.ceil((totalPrice)*100)/100);
-const bul = (integersTotalPrice % 2 === 0);
-const dealing = (money - totalPrice); // здача
-const averagePrice = ((milkPrice + winePrice + showergelPrice)/3).toFixed(2);
+const totalPrice = milkPrice + winePrice + showergelPrice; // загальна ціна
+const integersTotalPrice = Math.floor(milkPrice + winePrice + showergelPrice); // загальна ціна цілим числом
+const ceilprice = Math.ceil(totalPrice * 100) / 100;
+const bul = integersTotalPrice % 2 === 0;
+const dealing = money - totalPrice; // здача
+const averagePrice = ((milkPrice + winePrice + showergelPrice) / 3).toFixed(2);
+const discount = Math.floor(Math.random() * 50);
 
-const discount = (Math.floor(Math.random() * 50));
+const needbuy =
+  integersTotalPrice - (integersTotalPrice / 100) * discount.toFixed(2);
 
+const discountforpureStaff = (integersTotalPrice / 100) * discount;
 
-
-
+let pureStuffing = integersTotalPrice / 2 - discountforpureStaff;
 //------advanced part-------------------------------------
 
-let message = `
+message = `
   <h1>HW#1 Serhii Demchyshyn</h1>
   <p>Milk price - <strong>${milkPrice}</strong></p>
   <p>Wine Price - <strong>${winePrice}</strong></p>
@@ -33,6 +35,8 @@ let message = `
   <p>Boolean value - <strong>${bul}</strong></p>
   <p>Average Price - <strong>${averagePrice}</strong></p>
   <p>Your discount - <strong>${discount}</strong></p>
+  <p>Need buy - <strong>${needbuy}</strong></p>
+  <p>pureStuffing - <strong>${pureStuffing}</strong></p>
 `;
 document.writeln(message);
 console.log(message);
