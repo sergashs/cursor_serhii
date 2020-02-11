3fd68db28e3d5268e5be01ef41c2c8bff70c10c7
sergashs <sergas123456@gmail.com>
2020-02-11 21:36:57 +0200

    у

--- js/js/index.js
+++ js/js/index.js
@@ -1,13 +1,17 @@
 //2 = pow function
 
-function two() {
+// while(isNaN(namber)) {
+//   alert('Ви ввели не число');
+
+function getpercent() {
   let namber = document.getElementById("fun-2").value;
   let prcnt = document.getElementById("fun-22").value;
 
   namber = parseInt(namber);
   pow = parseInt(prcnt);
+
   let temp = (namber / 100) * prcnt;
-  document.getElementById("fun-2-label").innerHTML = temp;
-  document.getElementById("fun-2-label").innerHTML = temp;
-  two();
+
+  document.getElementById("fun-2-label").innerHTML = temp.toFixed();
+  getpercent();
 }
