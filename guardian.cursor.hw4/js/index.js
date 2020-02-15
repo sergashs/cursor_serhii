4c1fdb2af53e182dcd9099407f7b2251f54c089c
sergashs <sergas123456@gmail.com>
2020-02-15 18:10:27 +0200

    ы

--- guardian.cursor.hw4/js/index.js
+++ guardian.cursor.hw4/js/index.js
@@ -1,32 +1,25 @@
-// const students = ["Саша", "Игорь", "Лена", "Ира", "Алексей", "Светлана"];
-// const themes = [
-//   "Дифференциальные уравнения",
-//   "Теория автоматов",
-//   "Алгоритмы и структуры данных"
-// ];
-// const marks = [4, 5, 5, 3, 4, 5];
+const students = ["Саша", "Игорь", "Лена", "Ира", "Алексей", "Светлана"];
+const themes = [
+  "Дифференциальные уравнения",
+  "Теория автоматов",
+  "Алгоритмы и структуры данных"
+];
+const marks = [4, 5, 5, 3, 4, 5];
 
-// let pairs = [];
-// const putStudentToPair = function(arr) {
-//   let j = 0;
-//   for (let i = 0; i < arr.length; i += 2) {
-//     pairs[j] = arr.slice(i, i + 2);
-//     j++;
-//   }
-//   return pairs;
-// };
-// console.log(putStudentToPair(students));
+const studentsPairs = [
+  students[0],
+  students[3],
+  students[1],
+  students[2],
+  students[4],
+  students[5]
+];
 
-// document.write(
-//   "<h4>1) Вийшли отакі пари з потенційних хлопчиків та дівчаток:</h4>"
-// );
-// for (let i = 0; i < pairs.length; i++) {
-//   document.write(`<p>Пара ${i + 1}: ${pairs[i]}</p>`);
-// }
+document.writeln(`
+<p><b>Студенти:</b>  ${students}</p>
 
-let i = 1;
-while (i < 3) {
-  // выводит 0, затем 1, затем 2
-  alert(i);
-  i++;
-}
+<b>Вийшли такі пари:</b>
+<p>1. ${studentsPairs[0]} + ${studentsPairs[3]}</p>
+<p>2. ${studentsPairs[1]} + ${studentsPairs[2]}</p>
+<p>3. ${studentsPairs[4]} + ${studentsPairs[5]}</p>
+`);
