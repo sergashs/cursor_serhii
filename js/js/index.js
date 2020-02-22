3f33bcc31291463f3daa477cc4629ff28819c822
sergashs <sergas123456@gmail.com>
2020-02-23 00:18:58 +0200

    ы

--- js/js/index.js
+++ js/js/index.js
@@ -1,33 +1,35 @@
-//2 = pow function
+2 = pow function
 
-// while(isNaN(namber)) {
-//   alert('Ви ввели не число');
+while(isNaN(namber)) {
+  alert('Ви ввели не число');
 
-// function onInputHandler(e) {
-//   if (isNaN(e.target.value)) {
-//     e.target.classList.add("invalid");
-//   } else {
-//     e.target.classList.remove("invalid");
-//   }
-// }
-
-// function getpercent() {
-//   let namber = document.getElementById("fun-2").value;
-//   let prcnt = document.getElementById("fun-22").value;
-
-//   namber = parseInt(namber);
-//   pow = parseInt(prcnt);
-
-//   let temp = (namber / 100) * prcnt;
-
-//   document.getElementById("fun-2-label").innerHTML = temp.toFixed();
-//   getpercent();
-// }
-
-function count() {
-  for (let i = 10; i > 0; i--) {
-    console.log("hello " + i);
+function onInputHandler(e) {
+  if (isNaN(e.target.value)) {
+    e.target.classList.add("invalid");
+  } else {
+    e.target.classList.remove("invalid");
   }
 }
 
-count();
+function getpercent() {
+  let namber = document.getElementById("fun-2").value;
+  let prcnt = document.getElementById("fun-22").value;
+
+  namber = parseInt(namber);
+  pow = parseInt(prcnt);
+
+  let temp = (namber / 100) * prcnt;
+
+  document.getElementById("fun-2-label").innerHTML = temp.toFixed();
+  getpercent();
+}
+
+// function count() {
+//   for (let i = 10; i > 0; i--) {
+//     if (i % 2 == 0) {
+//       console.log("hello " + i);
+//     }
+//   }
+// }
+
+// count();
