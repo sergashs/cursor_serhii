2514db9f021e756d4e08deeed2e180600b034c49
sergashs <sergas123456@gmail.com>
2020-02-28 21:16:05 +0200

    ik

--- chat/js/index.js
+++ chat/js/index.js
@@ -21,16 +21,15 @@ function sendMesssage() {
   var newdiv = document.createElement("div");
   let length = document.getElementById("getmessage").value;
   let temp = length;
-  var date = new Date();
+  var now = new Date().toLocaleTimeString();
 
   newdiv.innerHTML = `
-
   <div class="message-container">
   <div class="message-left-side">
-   <div class="avatar-chat"><img src="http://127.0.0.1:5500/img/ava.jfif" alt="avatar-chat"></div>
+  <div class="avatar-chat"><img src="http://127.0.0.1:5500/img/ava.jfif" alt="avatar-chat"></div>
   </div>
   <div class="message-content">
-  <div class="message-title">Serhii</div>
+  <div class="message-title">Serhii <span>${now}</span></div>
   <div class="message">${temp}</div>
   </div>
   </div>
