d85e830c31c765f6eb28465b36f5c8606e5824af
sergashs <sergas123456@gmail.com>
2020-03-15 00:30:53 +0200

    s

--- js/js/index.js
+++ js/js/index.js
@@ -20,12 +20,61 @@
 
 // count();
 
-var myCrazyObject = {
-  name: "Нелепый объект",
-  "some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3],
-  "random animal": "Банановая акула"
-};
+/* вибираєм який суп варити, рандом
+// function getRandsup(max, min) {
+//   let soups = ["борщ", "гречаний", "сосиками"];
+//   min = 0;
+//   max = soups.length;
+//   return soups[Math.floor(Math.random() * (max - min)) + min];
+// }
+// function getRandsupPrint() {
+//   let temp = getRandsup();
+//   document.getElementById("fun-3-label").innerHTML = temp;
+// }
+/* кінець рандом функції */
 
-// console.log(myCrazyObject["some array"]);
+/* робимо тварин прекрасними
+// let animals = ["Кот", "Рыба", "Лемур", "Комодский варан"];
 
-console.log(Array.from(myCrazyObject["some array"]));
+// for (let i = 0; i < animals.length; i++) {
+//   console.log(animals[i] + " прекрасное животное");
+// }
+/* кінець прекрасних тварин */
+
+/* генератор стрічки з рандомними буквами */
+
+// let alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+// let randomString = "";
+// let counter = 0;
+
+// while (counter < 6) {
+//   let bukva = alphabet[Math.floor(Math.random() * alphabet.length)];
+//   randomString += bukva;
+//   counter++;
+// }
+
+// console.log(randomString);
+
+function hackertextmade(input) {
+  let output = "";
+  for (i = 0; i < input.length; i++) {
+    if (input[i] == "a") {
+      output += 4;
+    } else if (input[i] == "e") {
+      output += 3;
+    } else if (input[i] == "i") {
+      output += "l";
+    } else if (input[i] == "o") {
+      output += "0";
+    } else {
+      output += input[i];
+    }
+  }
+  return output;
+}
+
+function hackertextmadePrint() {
+  let input = document.getElementById("funcget4").value.toLowerCase();
+  let temp = hackertextmade(input);
+  document.getElementById("function4").innerHTML += `${temp.split(" ")}`;
+}
