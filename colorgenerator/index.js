aff7b8b1b5b33334672a516919eb3a1bb828bb07
sergashs <sergas123456@gmail.com>
2020-03-20 20:46:56 +0200

    s

--- colorgenerator/index.js
+++ colorgenerator/index.js
@@ -5,38 +5,58 @@ function generatecolor() {
   return "#" + result;
 }
 
-// this can be any color
-pickTextColorBasedOnBgColorSimple(
-  document.body.style.color,
-  "#FFFFFF",
-  "#000000"
-);
-function pickTextColorBasedOnBgColorSimple(bgColor, lightColor, darkColor) {
-  var color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
-  var r = parseInt(color.substring(0, 2), 16); // hexToR
-  var g = parseInt(color.substring(2, 4), 16); // hexToG
-  var b = parseInt(color.substring(4, 6), 16); // hexToB
-  result = r * 0.399 + g * 0.687 + b * 0.214 > 186 ? darkColor : lightColor;
-  return result;
-}
-
-function printcolor() {
-  document.getElementById(
-    "scriptColor"
-  ).innerText = color = document.body.style.backgroundColor = generatecolor();
-
-  document.body.style.color = pickTextColorBasedOnBgColorSimple(
-    color,
-    "#FFFFFF",
-    "#000000"
-  );
-}
-
-document.addEventListener("keypress", function(event) {
-  if (event.keyCode == 13) {
-    printcolor();
+if (document.documentElement.clientWidth < 768) {
+  document.getElementById("scriptColor").innerHTML = "Touch to <b>screen</b>";
+  console.log("mobile");
+  function pickTextColorBasedOnBgColorSimple(bgColor, lightColor, darkColor) {
+    var color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
+    var r = parseInt(color.substring(0, 2), 16); // hexToR
+    var g = parseInt(color.substring(2, 4), 16); // hexToG
+    var b = parseInt(color.substring(4, 6), 16); // hexToB
+    result = r * 0.399 + g * 0.687 + b * 0.214 > 186 ? darkColor : lightColor;
+    return result;
   }
-});
+
+  document.body.onclick = function() {
+    document.getElementById(
+      "scriptColor"
+    ).innerText = color = document.body.style.backgroundColor = generatecolor();
+
+    document.body.style.color = pickTextColorBasedOnBgColorSimple(
+      color,
+      "#FFFFFF",
+      "#000000"
+    );
+  };
+} else {
+  console.log("pc");
+  function pickTextColorBasedOnBgColorSimple(bgColor, lightColor, darkColor) {
+    var color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
+    var r = parseInt(color.substring(0, 2), 16); // hexToR
+    var g = parseInt(color.substring(2, 4), 16); // hexToG
+    var b = parseInt(color.substring(4, 6), 16); // hexToB
+    result = r * 0.399 + g * 0.687 + b * 0.214 > 186 ? darkColor : lightColor;
+    return result;
+  }
+
+  function printcolor() {
+    document.getElementById(
+      "scriptColor"
+    ).innerText = color = document.body.style.backgroundColor = generatecolor();
+
+    document.body.style.color = pickTextColorBasedOnBgColorSimple(
+      color,
+      "#FFFFFF",
+      "#000000"
+    );
+  }
+
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode == 13) {
+      printcolor();
+    }
+  });
+}
 
 // document.body.style.color = "#ffffff";
 
