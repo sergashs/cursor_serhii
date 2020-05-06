b8f89c9821b803251220a7803d190919f0e94d69
sergashs <sergas123456@gmail.com>
2020-05-06 20:25:13 +0300

    make better modal window, add working close button and optimized css style

--- game/index.js
+++ game/index.js
@@ -173,40 +173,88 @@ else {
   );
 }
 
-///// popup
-const btns = document.querySelectorAll(".open-modal");
-const modalOverlay = document.querySelector(".modal-overlay ");
-const modals = document.querySelectorAll(".modal");
-const IconcloseModals = document.querySelector(".close-modal-icon ");
+!(function (e) {
+  "function" != typeof e.matches &&
+    (e.matches =
+      e.msMatchesSelector ||
+      e.mozMatchesSelector ||
+      e.webkitMatchesSelector ||
+      function (e) {
+        for (
+          var t = this,
+            o = (t.document || t.ownerDocument).querySelectorAll(e),
+            n = 0;
+          o[n] && o[n] !== t;
 
-btns.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    let path = e.currentTarget.getAttribute("data-path");
+        )
+          ++n;
+        return Boolean(o[n]);
+      }),
+    "function" != typeof e.closest &&
+      (e.closest = function (e) {
+        for (var t = this; t && 1 === t.nodeType; ) {
+          if (t.matches(e)) return t;
+          t = t.parentNode;
+        }
+        return null;
+      });
+})(window.Element.prototype);
 
-    modals.forEach((el) => {
-      el.classList.remove("modal--visible");
+document.addEventListener("DOMContentLoaded", function () {
+  /* Записываем в переменные массив элементов-кнопок и подложку.
+      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+  var modalButtons = document.querySelectorAll(".open-modal"),
+    overlay = document.querySelector(".overlay"),
+    closeButtons = document.querySelectorAll(".close-modal-icon");
+
+  /* Перебираем массив кнопок */
+  modalButtons.forEach(function (item) {
+    /* Назначаем каждой кнопке обработчик клика */
+    item.addEventListener("click", function (e) {
+      /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+      e.preventDefault();
+
+      /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+      var modalId = this.getAttribute("data-modal"),
+        modalElem = document.querySelector(
+          '.modal[data-modal="' + modalId + '"]'
+        );
+
+      /* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+      modalElem.classList.add("active");
+      overlay.classList.add("active");
+    }); // end click
+  }); // end foreach
+
+  closeButtons.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      var parentModal = this.closest(".modal");
+
+      parentModal.classList.remove("active");
+      overlay.classList.remove("active");
     });
+  }); // end foreach
 
-    document
-      .querySelector(`[data-target="${path}"]`)
-      .classList.add("modal--visible");
-    modalOverlay.classList.add("modal-overlay--visible");
+  document.body.addEventListener(
+    "keyup",
+    function (e) {
+      var key = e.keyCode;
+
+      if (key == 27) {
+        document.querySelector(".modal.active").classList.remove("active");
+        document.querySelector(".overlay").classList.remove("active");
+      }
+    },
+    false
+  );
+
+  overlay.addEventListener("click", (e) => {
+    if (e.target == overlay) {
+      document.querySelector(".overlay").classList.remove("active");
+    }
   });
-});
-
-modalOverlay.addEventListener("click", (e) => {
-  console.log(e.target);
-  if (e.target == modalOverlay) {
-    modalOverlay.classList.remove("modal-overlay--visible");
-    modals.forEach((el) => {
-      el.classList.remove("modal--visible");
-    });
-  }
-
-  if (e.target == IconcloseModals) {
-    modalOverlay.classList.remove("modal-overlay--visible");
-    modals.forEach((el) => {
-      el.classList.remove("modal--visible");
-    });
-  }
-});
+}); // end ready
