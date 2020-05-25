a71713ddf993b7fac3493e411d46cc0be71915ea
sergashs <sergas123456@gmail.com>
2020-05-26 00:49:54 +0300

    add portfolio temple

---
+++ portfolio/js/index.js
@@ -0,0 +1,41 @@
+document.getElementById("darkmode").addEventListener("change", (ev) => {
+  let btn = ev.target;
+
+  if (btn.checked) {
+    localStorage.setItem("darkmode", "on");
+    document.querySelector(".icon-moon").innerHTML =
+      ' <img src="./img/moon-on.svg" />';
+    document.documentElement.style.setProperty("--body-background", "#222325");
+    document.documentElement.style.setProperty("--text", "#adadad");
+    document.documentElement.style.setProperty("--grey-text", "#6b6b6b");
+    document.documentElement.style.setProperty("--input--border", "#6b6b6b");
+  } else {
+    localStorage.setItem("darkmode", "off");
+    document.querySelector(".icon-moon").innerHTML =
+      ' <img src="./img/moon.svg" />';
+    document.documentElement.style.setProperty("--body-background", "#f2f2f2");
+    document.documentElement.style.setProperty("--text", "#000000");
+    document.documentElement.style.setProperty("--grey-text", "#979797");
+    document.documentElement.style.setProperty("--input--border", "#cfcfcf");
+  }
+});
+
+if (localStorage.getItem("darkmode") == "on") {
+  document.querySelector(".icon-moon").innerHTML =
+    ' <img src="./img/moon-on.svg" />';
+  document.documentElement.style.setProperty("--body-background", "#222325");
+  document.documentElement.style.setProperty("--text", "#adadad");
+  document.documentElement.style.setProperty("--grey-text", "#6b6b6b");
+  document.documentElement.style.setProperty("--input--border", "#6b6b6b");
+}
+
+// по умолчанию светлая тема
+else {
+  localStorage.setItem("darkmode", "off");
+  document.querySelector(".icon-moon").innerHTML =
+    ' <img src="./img/moon.svg" />';
+  document.documentElement.style.setProperty("--body-background", "#f2f2f2");
+  document.documentElement.style.setProperty("--text", "#000000");
+  document.documentElement.style.setProperty("--grey-text", "#979797");
+  document.documentElement.style.setProperty("--input--border", "#cfcfcf");
+}
